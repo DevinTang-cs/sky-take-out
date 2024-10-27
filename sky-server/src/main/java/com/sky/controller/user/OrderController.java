5a58521c58e00598ae6e0b257262a9b54e265574
Devin Tang <3222561952@qq.com>
2024-10-27 15:43:44 +0800

    再来一单功能代码

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -84,4 +84,16 @@ public class OrderController {
         orderService.userCancelById(id);
         return Result.success();
     }
+
+    /**
+     * 再来一单
+     * @param id
+     * @return
+     */
+    @ApiOperation("再来一单")
+    @PostMapping("repetition/{id}")
+    public Result repetition(@PathVariable Long id){
+        orderService.repetition(id);
+        return Result.success();
+    }
 }

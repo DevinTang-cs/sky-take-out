f618f8ab1509eda329769ac9d4477a2aa5771c6f
Devin Tang <3222561952@qq.com>
2024-10-27 14:51:54 +0800

    历史订单查询功能代码

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -28,4 +30,7 @@ public interface OrderMapper {
 
     @Select("select * from sky_take_out.orders where number = #{orderNumber} and user_id= #{userId}")
     Orders getByNumberAndUserId(String orderNumber, Long userId);
+
+    
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
 }

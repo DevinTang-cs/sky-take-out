0744495e12191be7238ef32a759c7ddc70df7d9b
Devin Tang <3222561952@qq.com>
2024-10-29 10:07:50 +0800

    营业额统计代码

---
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -0,0 +1,17 @@
+package com.sky.service;
+
+import com.sky.vo.TurnoverReportVO;
+
+import java.time.LocalDate;
+
+public interface ReportService {
+
+    /**
+     * 指定时间内的营业额
+     * @param begin
+     * @param end
+     * @return
+     */
+    TurnoverReportVO getTurnoverStatistics(LocalDate begin,LocalDate end);
+
+}
